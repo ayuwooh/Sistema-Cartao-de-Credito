2eaae794ec0fcced9c6ec1a4510b77e5f204db63
ayuwooh <ayuwooh@gmail.com>
2026-09-03 00:52:50 -0300

    cases

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'modelos/compra.dart';
 import 'modelos/cartao_de_credito.dart';
 import 'servicos/servico_de_compra.dart';
 
@@ -15,10 +16,14 @@ void main() {
     switch (opcao) {
       case '1':
         stdout.write('Descricao: ');
-        String descricao = (stdin.readLineSync()!);
+        String descricao = stdin.readLineSync()!;
+
+        stdout.write('Valor: ');
+        double valor = double.parse(stdin.readLineSync()!);
 
         try {
-          bool aprovada = servico.realizarCompra(descricao: descricao, valor: valor);
+          Compra compra = Compra(descricao: descricao, valor: valor);
+          bool aprovada = servico.realizarCompra(cartao, compra);
           if (aprovada) {
             print('Compra aprovada!');
           } else {
@@ -29,6 +34,25 @@ void main() {
         }
         break;
       case '2':
+        print('=== Dados do Cartão ===');
+        print('Limite: ${cartao.limite}');
+        print('Valor utilizado: ${cartao.valorUtilizado}');
+        print('Saldo disponível: ${cartao.saldoDisponivel}');
+        print('Quantidade de compras: ${cartao.quantidadeCompras}');
+        break;
+      case '3':
+        if (cartao.comprasOrdenadas.isEmpty) {
+          print('Nenhuma compra realizada.');
+        } else {
+          print('=== Compras (por valor) ===');
+          for (Compra compra in cartao.comprasOrdenadas) {
+            print('${compra.descricao} - R\$ ${compra.valor}');
+          }
+        }
+        break;
+      case '4':
+        print('Encerrando o sistema.');
+        return;
     }
   }
 }
